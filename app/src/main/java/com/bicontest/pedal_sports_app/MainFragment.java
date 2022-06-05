613e582d669950c50ec1c9e29ae19a67876deb08
jung0115 <jungim7490@gmail.com>
2022-06-05 20:57:23 +0900

    수정

--- app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
+++ app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
@@ -7,12 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false); // fragment에서 findViewById를 하기 위한 세팅
+
+        final ScrollView scrollView = (ScrollView) view.findViewById(R.id.scrollView);
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(scrollView.FOCUS_UP);
+            }
+        });
+
+        return view;
     }
 }
