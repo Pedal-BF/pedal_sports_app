7ab4497f6f099eb59a25171e8f637330339df82a
jung0115 <jungim7490@gmail.com>
2022-06-16 15:53:30 +0900

    중간저장

--- app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
+++ app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
@@ -1,5 +1,6 @@
 package com.bicontest.pedal_sports_app;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -24,12 +27,25 @@ public class MainFragment extends Fragment {
     private JSONObject SeoulJSONObject;
     private JSONArray SeoulJSONArray;
 
+    private ImageView youtubeImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // fragment에서 findViewById를 하기 위한 세팅
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        //https://youtu.be/MfZf-hV9mPw
+        String youtubeThumbnail = "https://img.youtube.com/vi/MfZf-hV9mPw/default.jpg";
+        youtubeImage = view.findViewById(R.id.adImage);
+        try {
+            Uri uri = Uri.parse(youtubeThumbnail);
+            youtubeImage.setImageURI(uri);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*
         // 서울올림픽기념국민체육진흥공단_국민체력100_운동처방_동영상정보 API에서 JSON 형태로 데이터 받아오기
         try {
             URL url = new URL("https://api.odcloud.kr/api/15084814/v1/uddi:3f8d6b98-0082-4792-92a8-90d40ecc4bce?page=1&perPage=" + SeoulExerciseLimitPage +"&serviceKey=" + SeoulExerciseKey );
@@ -46,7 +62,7 @@ public class MainFragment extends Fragment {
 
         }catch(Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
         return view;
     }
