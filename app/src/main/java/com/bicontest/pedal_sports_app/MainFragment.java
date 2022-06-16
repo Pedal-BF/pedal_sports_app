71f7e01411f431ca768bbb40048a6d5aebd47b2c
jung0115 <jungim7490@gmail.com>
2022-06-16 15:28:56 +0900

    하단 메뉴바 그림자

--- app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
+++ app/src/main/java/com/bicontest/pedal_sports_app/MainFragment.java
@@ -4,15 +4,50 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class MainFragment extends Fragment {
+    // 서울올림픽기념국민체육진흥공단_국민체력100_운동처방_동영상정보
+    private String SeoulExerciseKey = "7VgAbrUNHG0BQOPUubAEEkOT45PoaRK6TR92eLuGBsfqyhspb%2BY1oOyrwqIeWXYGrSVw9vMreaGpnekwpR8pGw%3D%3D";   // 인증키 (개인이 받아와야함)
+    private String SeoulExerciseLimitPage = "248";  // 248
+    private String SeoulExerciseJson;        // 파싱한 데이터를 저장할 변수
+    private JSONObject SeoulJSONObject;
+    private JSONArray SeoulJSONArray;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        // fragment에서 findViewById를 하기 위한 세팅
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // 서울올림픽기념국민체육진흥공단_국민체력100_운동처방_동영상정보 API에서 JSON 형태로 데이터 받아오기
+        try {
+            URL url = new URL("https://api.odcloud.kr/api/15084814/v1/uddi:3f8d6b98-0082-4792-92a8-90d40ecc4bce?page=1&perPage=" + SeoulExerciseLimitPage +"&serviceKey=" + SeoulExerciseKey );
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            SeoulExerciseJson = bf.readLine();
+            bf.close();
+
+            SeoulJSONObject = new JSONObject(SeoulExerciseJson);
+            String contents = SeoulJSONObject.getString("data");
+            SeoulJSONArray = new JSONArray(contents);
+
+            Log.v("API", "log test");
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return view;
     }
 }
